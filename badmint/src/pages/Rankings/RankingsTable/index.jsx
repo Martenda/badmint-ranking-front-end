7c0903f3d33e3ddfa255a198622477f191a13949
Lucas Dolsan <lucas.dolsan1775@gmail.com>
2022-12-08 23:00:03 -0300

    fix(RankingsTable): (WIP) team result detail modal

--- badmint/src/pages/Rankings/RankingsTable/index.jsx
+++ badmint/src/pages/Rankings/RankingsTable/index.jsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Popover from "@mui/material/Popover";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridCloseIcon } from "@mui/x-data-grid";
+import TrophyIcon from '@mui/icons-material/EmojiEvents';
+import { Button, Card, Modal } from "@mui/material";
 
 const columns = [
   {
@@ -28,7 +29,7 @@ const columns = [
     field: "athletesNames",
     headerName: "Nome do Atleta/Dupla",
     renderCell: (cellValues) => {return (<label>{cellValues.row.athlete1Name}<br/>{cellValues.row.athlete2Name}</label>)},
-    width: 400,
+    width: 340,
     sortable: false,
     hideable: false,
     headerAlign: "center",
@@ -68,18 +69,13 @@ const columns = [
     description: "Pontos válidos do atleta no período em questão",
   },
   {
-    field: "testDescription",
-    headerName: "Test Description",
-    description: "This column has a value getter and is not sortable.",
-    width: 300,
+    headerName: "Resultados",
+    description: "Resultados da equipe por campeonato",
+    width: 15,
     sortable: false,
     hideable: false,
     headerAlign: "center",
-    valueGetter: (params) =>
-      `${params.row.athleteName || ""} testing ${
-        params.row.athleteMemberID || ""
-      }`,
-    hide: true,
+    // renderCell: (cellValues) => {return <TrophyIcon onClick={handleTrophyClick(cellValues.row.athlete1MemberID, cellValues.row.athlete2MemberID)} />},
   },
 ];
 
@@ -93,19 +89,100 @@ function rowHeightEval(rankingQueryResults) {
 }
 
 function RankingsTable({ rankingQueryResults }) {
-  const handlePopoverOpen = (event) => {};
+  
+  const [isResultModalOpen, setIsResultModalOpen] = React.useState(false);
+  
+  
+  function handleTrophyClick(athlete1MemberId, Athlete2MemberId) {
+    setIsResultModalOpen(true)
 
-  if(!rankingQueryResults || !rankingQueryResults.length) {
-    return <></>
+    alert(JSON.stringify({ athlete1MemberId, Athlete2MemberId }))
   }
+  
+  
+  const mock = [
+    {ChampionshipName: '2022 Etapa 1 Presidente Getúlio', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 2 Ibirama', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 3 Presidente Getúlio', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 4 Rio do Sul', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Florianópolis', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Florianópolis', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 2 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Presidente Getúlio', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 3 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Ibirama', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Presidente Getúlio', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 4 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Florianópolis', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Ibirama', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Florianópolis', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 2 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Rio do Sul', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 2 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Rio do Sul', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 12 Rio do Sul', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Presidente Getúlio', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Florianópolis', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+    {ChampionshipName: '2022 Etapa 1 Joaçaba', ChampionshipDate: '01/01/2022', ClassificationPosition: '4', ScorePoints: '800'},
+  ];
+
+  function renderModalRow(item) {
+    return `Campeonato ${item.ChampionshipName} em ${item.ChampionshipDate}: ${item.ClassificationPosition}° lugar - ${item.ScorePoints} pontos`
+  }
+
+  function renderRows(list) {
+    return list.map(e => {
+      return (
+        <Card sx={{marginBottom: '1rem', padding: '0.5rem', display: 'inline-block' }}>
+          {renderModalRow(e)}
+        </Card>
+      )
+    })
+  }
+
+  // Campeonato ${ChampionshipName} em ${ChampionshipDate}: ${ClassificationPosition}° lugar - ${ScorePoints} pontos
+  return (
+    <div>
+    <TrophyIcon onClick={() => handleTrophyClick(1,2)} />
+
+    <Modal
+      open={isResultModalOpen}
+      sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+      onClose={()=>{}}
+      aria-labelledby="parent-modal-title"
+      aria-describedby="parent-modal-description"
+      >
+      <Box sx={{ width: '70%', height: '90%', backgroundColor: 'white', overflowY: 'scroll', borderRadius: '1rem', padding: '1rem' }}>
+        <div style={{ display: 'flex'}}>
+          <h2 id="parent-modal-title">Resultados da equipe por campeonato</h2>
+          <Button onClick={()=>setIsResultModalOpen(false)}>
+            <GridCloseIcon />
+        </Button>
+        </div>
+        {renderRows(mock)}
+      </Box>
+    </Modal>
+  </div>
+  )
+
+
+  
+  // if(!rankingQueryResults || !rankingQueryResults.length) {
+  //   return <></>
+  // }
 
   return (
     <Box
-      sx={{
-        height: "100vh",
-        width: "100%",
-        marginTop: 1,
-      }}
+    sx={{
+      height: "100vh",
+      width: "100%",
+      marginTop: 1,
+    }}
     >
     <DataGrid
       rows={rankingQueryResults}
@@ -116,19 +193,8 @@ function RankingsTable({ rankingQueryResults }) {
       disableColumnMenu
       disableSelectionOnClick
       experimentalFeatures={{ newEditingApi: true }}
-      componentsProps={{
-        cell: {
-          onMouseEnter: handlePopoverOpen,
-        },
-      }}
     />
-      <Popover
-        sx={{
-          pointerEvents: "none",
-        }}
-        //open={open}
-        disableRestoreFocus
-      ></Popover>
+
     </Box>
   );
 }
